6b857504a8bf43e8b3b8712966d45b09deddc480
jieun <jieun3001@gmail.com>
2018-04-12 10:16:14 +0900

    서버 이메일 인증 추가

--- BACKEND/ColleckingSeoul/routes/join.js
+++ BACKEND/ColleckingSeoul/routes/join.js
@@ -154,4 +154,102 @@ router.get('/check_dupplicate', function (req, res) {
     async.waterfall(task, globalModule.asyncCallback.bind(this));
 });
 
+/**
+ * api 목적        : email 인증
+ * request params : tempEmail(query)
+ */
+
+router.get('/verificationCode', function (req, res) {
+
+    var Transport = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: mailConfig.jieun.user,
+            pass: mailConfig.jieun.pass
+        }
+    });
+
+    var rand;
+    var resultJson = {
+        message: '',
+        verificationCode: ''
+    };
+
+    var connect = function (callback) {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log("getConnection error : ", err);
+                callback(err, connection, null);
+            }
+            else callback(null, connection);
+        });
+    }
+
+    var selectUserInfo = function (connection, callback) {
+        let duplicate_check_query = "select * from User where id = ?";
+        connection.query(duplicate_check_query, req.query.tempEmail, function (err, data) {
+            if (err) {
+                console.log("duplicate check select query error : ", err);
+                callback(err, connection, null);
+            } else {
+                if (data.length == 0) {
+                    callback(null, connection);
+                }
+                else {
+                    resultJson.message = "duplicated";
+                    resultJson.detail = "unable to sign up";
+                    res.status(201).send(resultJson);
+                    callback('ok', connection);
+                }
+            }
+        });
+    }
+
+    var sendMail = function (connection, callback) {
+        rand = Math.floor((Math.random() * 10000));
+        let mailOption = {
+            to: req.query.tempEmail,
+            subject: "안녕하세요. safe,save! 입니다.",
+            html: "안녕하세요,<br> 고객님의 인증번호는 " + rand + "입니다. <br>"
+            + "<br>어플로 돌아가셔서 인증번호를 입력해 주세요.</br>"
+            + "<br>감사합니다.</br>"
+        };
+        Transport.sendMail(mailOption, function (error, info) {
+            if (error) {
+                return console.log(error);
+            } else {
+                resultJson.message = 'email success';
+                resultJson.verificationCode = rand;
+                res.status(201).send(resultJson);
+                callback(null, connection);
+
+            }
+        });
+    }
+
+    var releaseConnection = function (connection, callback) {
+        connection.release();
+        callback(null, null, '-verificationCode');
+    }
+
+    var verificationCode_task = [connect, selectUserInfo, sendMail, releaseConnection];
+
+    async.waterfall(verificationCode_task, function (err, connection, result) {
+        if (connection) {
+            connection.release();
+        }
+        if (err) {
+            if (err != 'ok') {
+                console.log("async.waterfall error : ", err);
+                res.status(503).send({
+                    message: 'failuire',
+                    detail: 'internal server error'
+                });
+            }
+        }
+        else {
+            console.log(result);
+        }
+    });
+});
 module.exports = router;

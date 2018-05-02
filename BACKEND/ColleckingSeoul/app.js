670cdd6fb1bbe3acb8997fdb047a64308569a0b9
jieun <jieun3001@gmail.com>
2018-05-02 20:28:51 +0900

    서버 글 등록하기 구현

--- BACKEND/ColleckingSeoul/app.js
+++ BACKEND/ColleckingSeoul/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var joinRouter = require('./routes/join');
+var postRouter = require('./routes/post');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/join', joinRouter);
+app.use('/post', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

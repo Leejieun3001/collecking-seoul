1b02574c1171194626e1fc3124292eb689b7c43d
jieun <jieun3001@gmail.com>
2018-05-17 10:58:31 +0900

    안드로이드 마이페이지 레이아웃 구현

--- FRONTEND/ColleckingSeoul/app/src/main/java/kr/ac/sungshin/colleckingseoul/mypage/MyPageFragment.java
+++ FRONTEND/ColleckingSeoul/app/src/main/java/kr/ac/sungshin/colleckingseoul/mypage/MyPageFragment.java
@@ -1,19 +1,31 @@
 package kr.ac.sungshin.colleckingseoul.mypage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import kr.ac.sungshin.colleckingseoul.R;
+import kr.ac.sungshin.colleckingseoul.home.HomeActivity;
 
 /**
  * Created by kwonhyeon-a on 2018. 5. 15..
  */
 
 public class MyPageFragment extends Fragment {
+    @BindView(R.id.mypage_textview_modify)
+    TextView textViewModify;
+    @BindView(R.id.mypage_textview_logout)
+    TextView textViewLogout;
+    @BindView(R.id.mypage_textview_secession)
+    TextView textSecession;
+
     public MyPageFragment() {
         super();
     }
@@ -21,7 +33,17 @@ public class MyPageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_mypage, container, false);
+        ButterKnife.bind(this, view);
+        textViewModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
         return view;
     }
 }
---
+++ FRONTEND/ColleckingSeoul/app/src/main/java/kr/ac/sungshin/colleckingseoul/mypage/MyPageModifyActivity.java
@@ -0,0 +1,15 @@
+package kr.ac.sungshin.colleckingseoul.mypage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.ac.sungshin.colleckingseoul.R;
+
+public class MyPageModifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page_modify);
+    }
+}
